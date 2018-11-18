ae31909c8990b45303a41dbec0e31f37339bd48f
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-11-18 12:43:48 -0500

    Routes done?

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,41 @@
-// Import Express & the Model
+// Import Express & Router
 const express = require('express');
 const router = express.Router();
 
+// Import the Model (burger.js) to use it's database functions.
 const burger = require('../models/burger');
+
+// Create all the routes and the logic for those routes.
+router.get('/', function (req, res) {
+    burger.selectAll(function(data) {
+        const hbsObject = {
+            burgers: data
+        };
+        console.log(JSON.stringify(hbsObject, 2, null));
+        res.render('index', hbsObject);
+    });
+});
+
+router.post('/api/burgers', function(req, res){
+    burger.insertOne(['name'], [req.body.name], function(result) {
+        res.json({id: result.insertId});
+    });
+});
+
+router.put('/api/burgers/:id', function(req, res) {
+    const name = `id = ${req.params.id}`;
+    console.log('name', name);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, name, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end()
+        } else {
+            res.status(200).end()
+        }
+    });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
