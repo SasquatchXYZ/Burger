e912dbad29ed78e0322a1453817664a502c30376
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-11-18 21:20:23 -0500

    but will any of it really work...?

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,7 +17,7 @@ router.get('/', function (req, res) {
 });
 
 router.post('/api/burgers', function(req, res){
-    burger.insertOne(['name'], [req.body.name], function(result) {
+    burger.insertOne(['burger_name'], [req.body.burger_name], function(result) {
         res.json({id: result.insertId});
     });
 });
