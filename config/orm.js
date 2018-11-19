0024f983cd4239bd915ae2e4c512aa63d0442d53
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-11-19 02:39:31 -0500

    added more comments and cleaned up code

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,10 @@
 // Import MySQL Connection
 const connection = require('./connection');
 
+// Configure our 'Object-Relational Mapper'
 const orm = {
+
+    // Query to Retrieve all of the burgers in the database.
     selectAll: function (cb) {
         const query = 'SELECT * FROM burgers';
         connection.query(query, function (err, results) {
@@ -10,6 +13,7 @@ const orm = {
         });
     },
 
+    // Query to Add a new burger to the database, it receives the name of the burger from the Model.
     insertOne: function (burger_name, cb) {
         const query = `INSERT INTO burgers (burger_name) VALUES (?)`;
         connection.query(query, [burger_name], function (err, result) {
@@ -18,6 +22,8 @@ const orm = {
         });
     },
 
+    // Query to update a burger in the database, to change it's status from 'not devoured' to 'devoured'.
+    // It receives the ID of the burger to be updated from the Model.
     updateOne: function (id, cb) {
         const query = `UPDATE burgers SET devoured = 1 WHERE ${id}`;
         connection.query(query, function (err, result) {
--- models/burger.js
+++ models/burger.js
@@ -2,12 +2,15 @@
 const orm = require('../config/orm');
 
 const burger = {
+
+    // Function to Select All the burgers in the database; it simply invokes the SelectAll ORM Query.
     selectAll: function (cb) {
         orm.selectAll(function (res) {
             cb(res);
         });
     },
 
+    // Function to Add a burger to the database, it passes the name of the burger to the ORM Insert One Query.
     insertOne: function (burger_name, cb) {
         console.log(burger_name);
         orm.insertOne(burger_name, function (res) {
@@ -15,6 +18,7 @@ const burger = {
         });
     },
 
+    // Function to Update a burger in the database, it passes the ID of the burger to be updated to the ORM Query.
     updateOne: function (id, cb) {
         console.log(id);
         orm.updateOne(id, function (res) {
